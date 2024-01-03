5dfda8c0d336fe08aa1a0c40e14c5ed16629874a
suganyarajes <154896121+suganyarajes@users.noreply.github.com>
2024-01-02 23:37:33 -0600

    Update task2.js

--- task2.js
+++ task2.js
@@ -3,7 +3,7 @@ request.open("GET","https://restcountries.com/v2/all");
 request.send();
 request.onload = function(){
     var result = JSON.parse(request.response);
-    console.log(result);
+   // console.log(result);
 
     
 
