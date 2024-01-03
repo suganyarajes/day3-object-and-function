2b9c5028e9a520b45f5c1f1f2e06b28f320276cc
suganyarajes <154896121+suganyarajes@users.noreply.github.com>
2024-01-02 22:40:40 -0600

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,25 @@
+//how to compare two JSON have the same properties without order
+let obj1 ={
+    name:"rithvik",
+    age:5
+};
+
+let obj2 ={
+    age:5,
+    name:"rithvik"
+};
+
+for(var a in obj1){
+    console.log(a, obj1[a]); 
+
+    for(var b in obj2){
+        console.log(b, obj2[b]); 
+     }
+ }
+if(a==b){
+    console.log("JSON have the same properties");
+  }
+  else {
+    console.log("JSON not the same properties");
+  }
+
---
+++ task3.js
@@ -0,0 +1,13 @@
+var request =new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v2/all");
+request.send();
+request.onload = function(){
+    var result = JSON.parse(request.response);
+    //console.log(result);
+    for (var a in result){
+        console.log(a, result[a]);
+    }
+
+
+   
+}
