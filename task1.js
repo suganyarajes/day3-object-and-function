93bcb6299b1fe6addcb47d672fac84cf5da912bb
suganya <154896121+suganyarajes@users.noreply.github.com>
2024-03-16 23:15:46 -0500

    task1.js

--- task1.js
+++ task1.js
@@ -1,26 +1,33 @@
-//how to compare two JSON have the same properties without order
-let obj1 ={
-    name:"rithvik",
-    age:5
+let obj1 = {
+    name: "rithvik",
+    age: 5
 };
 
-let obj2 ={
-    age:5,
-    name:"rithvik"
+let obj2 = {
+    age: 5,
+    name: "rithvik"
 };
 
-for(var a in obj1){
-    console.log(a, obj1[a]); 
+let sameProperties = true;
 
-    for(var b in obj2){
-        console.log(b, obj2[b]); 
-     }
- }
-if(a==b){
-    console.log("JSON have the same properties");
-  }
-  else {
-    console.log("JSON not the same properties");
-  }
+for (var a in obj1) {
+    console.log(a, obj1[a]);
 
+    for (var b in obj2) {
+        console.log(b, obj2[b]);
 
+        if (a == b) {
+            console.log("Both objects have the same property:", a);
+            if (obj1[a] !== obj2[b]) {
+                console.log("But their values are different.");
+                sameProperties = false;
+            }
+        }
+    }
+}
+
+if (sameProperties) {
+    console.log("Both objects have the same properties.");
+} else {
+    console.log("Objects do not have the same properties or their values are different.");
+}
